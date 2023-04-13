b1ad27fc7267b7d3582a432b44bc61a953ab5a32
mfazaa <m666245@gmail.com>
2023-04-13 09:29:36 +0200

    cross browser test run

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -2,6 +2,12 @@ package Runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import stepDef.Hook;
+
+import java.io.IOException;
 
 
 @CucumberOptions(
@@ -17,5 +23,15 @@ import io.cucumber.testng.CucumberOptions;
 )
 
 public class TestRunner extends AbstractTestNGCucumberTests {
+        @Parameters({"browserName"})
+        @BeforeTest
+        public void beforeTest(String browserName) throws InterruptedException {
+                Hook.setBrowser(browserName);
+        }
 
+        @AfterTest
+        public  void CloseEmulator() throws IOException {
+
+
+        }
 }
--- src/test/java/stepDef/Hook.java
+++ src/test/java/stepDef/Hook.java
@@ -13,19 +13,43 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 public class Hook {
     public static WebDriver driver ;
+    private static String browser;
+
+    public static void  setBrowser(String browser) {
+        Hook.browser = browser;
+    }
+
     @Before
     public void openBrowser(){
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*","ignore-certificate-errors");
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver(chromeOptions);
+        if(browser.equals("Chrome")){
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--remote-allow-origins=*","ignore-certificate-errors");
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver(chromeOptions);
+        }
+        else if(browser.equals("Firefox")){
+         FirefoxOptions firefoxOptions = new FirefoxOptions();
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver(firefoxOptions);
+        }
+        else if(browser.equals("Edge")){
+            EdgeOptions edgeOptions = new EdgeOptions();
+            edgeOptions.addArguments("--remote-allow-origins=*","ignore-certificate-errors");
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver(edgeOptions);
+        }
+
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @AfterStep
